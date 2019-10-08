e97683b0d92476cb8dd62908560f74c79a08561d
119110382 ANNA BEATRIZ LUCENA LIRA <annabll@lcc3-009.lcc.ufcg.edu.br>
2019-10-08 08:07:27 -0300

    caso exponencial

---
+++ exponencial.py
@@ -0,0 +1,6 @@
+
+elif opcao == "6":
+n1 = float(input("\nDigite o primeiro número: "))
+        n2 = float(input("Digite o segundo número: "))
+        exponencial = (n1**n2)
+        print("\nResultado:", exponencial, "\n")
